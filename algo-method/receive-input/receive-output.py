28493c694969dd754fcac0fc3b8adaf3df761af7
shun198 <shunhiroseluvmri@gmail.com>
2022-08-06 11:32:45 +0900

    :+1: add receive-output

---
+++ algo-method/receive-input/receive-output.py
@@ -0,0 +1,22 @@
+# 標準出力1-1
+N = int(input())
+if N >= 1 and N <= 100:
+    print(int(N)*2)
+
+# 標準出力1-2
+N = int(input())
+if N >= 1 and N <= 100:
+    print(N % 5)
+
+# 標準出力1-3
+N = input()
+if len(N) >= 1 and len(N) <= 100:
+    print(N*3)
+
+# 標準出力1-4
+S = input()
+print(S[2])
+
+# 標準出力1-5
+X = input()
+print(24-int(X))
