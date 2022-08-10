94e610f447680b131ec2797d4499f03f38af0fdf
s-hirose <shunichi_hirose@pj100.biz>
2022-08-10 14:08:31 +0900

    :+1: add search-string.py 4

--- algo-method/search/search-string.py
+++ algo-method/search/search-string.py
@@ -29,3 +29,27 @@ if flag:
     print("Yes")
 else:
     print("No")
+
+# 全探索3
+# データを受け取る
+S = input()
+
+# S の長さを取得する
+N = len(S)
+
+# 線形探索 (0 から N-2 まで)
+count = 0
+for i in range(N - 1):
+    if S[i] == S[i + 1]:
+        count += 1
+print(count)
+
+# 全探索4
+N = int(input())
+S = input()
+T = input()
+count = 0
+for i in range(N):
+    if S[i] != T[i]:
+        count += 1
+print(count)
