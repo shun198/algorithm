225f449ea48737c177dbd9df38ce371e11b3b1b1
s-hirose <shunichi_hirose@pj100.biz>
2022-08-17 08:19:27 +0900

    :+1: add multiple-lists

--- algo-method/search/multiple-lists/chapter2.py
+++ algo-method/search/multiple-lists/chapter2.py
@@ -1,10 +1,9 @@
-N, M = map(int, input().split())
+N, M, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-
 cnt = 0
 for a_item in A:
     for b_item in B:
-        if a_item > b_item:
+        if a_item + b_item == K:
             cnt += 1
 print(cnt)
