ac72d90c027599d8174162700cde7cfb8de6cc2a
s-hirose <shunichi_hirose@pj100.biz>
2022-08-18 08:29:36 +0900

    :+1: add pairs from chapter1 to 2

--- algo-method/search/pairs/chapter2.py
+++ algo-method/search/pairs/chapter2.py
@@ -1,13 +1,12 @@
 # データを受け取る
-N, K = map(int, input().split())
-A = list(map(int, input().split()))
+L, R = map(int, input().split())
 
 count = 0
 # 変数 i の全探索
-for i in range(N):
+for i in range(L, R + 1):
     # 変数 j の全探索
-    for j in range(i + 1, N):
-        if A[i] + A[j] <= K:
+    for j in range(i + 1, R + 1):
+        if i % 10 == j % 10:
             count += 1
 
 # 答えを出力する
