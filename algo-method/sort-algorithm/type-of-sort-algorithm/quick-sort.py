7318d51bb046beeefd178a669b73215d3c6c5b44
s-hirose <shunichi_hirose@pj100.biz>
2022-08-25 08:16:44 +0900

    :+1: add quick sort

---
+++ algo-method/sort-algorithm/type-of-sort-algorithm/quick-sort.py
@@ -0,0 +1,35 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+# クイックソート本体
+def quick_sort(v):
+    # 配列が空の場合は何も起こらない
+    if len(v) == 0:
+        return []
+
+    # v[X] の値を軸に配列を分割する
+    X = len(v) // 2
+    L, R = [], []
+    for i, element in enumerate(v):
+        if i == X:
+            continue
+        if element < v[X]:
+            L.append(element)
+        else:
+            R.append(element)
+
+    # L, R を再帰的にソートする
+    L = quick_sort(L)
+    R = quick_sort(R)
+
+    # L, v[X], R をこの順につなげ、もとの配列を更新する
+    L.append(v[X])
+    L.extend(R)
+    return L
+
+
+# クイックソート
+A = quick_sort(A)
+
+# 配列の中身を出力する
+print(*A)
