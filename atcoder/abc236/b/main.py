6790e4824352a463373e58e5815a92a300b336fb
s-hirose <shunichi_hirose@pj100.biz>
2022-09-22 12:29:03 +0900

    :+1: abc236のaからc問題を追加

--- atcoder/abc236/b/main.py
+++ atcoder/abc236/b/main.py
@@ -1,1 +1,27 @@
-# コードを記載
+# # コードを記載
+N = int(input())
+A = list(map(int, input().split()))
+# # A_set = set(A)
+# # for items in A_set:
+# #     if A.count(items) != 4:
+# #         print(items)
+# #         break
+count = [0] * N
+for x in A:
+    count[x - 1] += 1
+
+print(count.index(3) + 1)
+# num1 = [1, 3, 2, 5, 4]
+# num2 = {1, 3, 6}
+# for num in num2:
+#     if num in num1:
+#         print("Yes")
+#     else:
+#         print("No")
+# algo1 = ["a", "l", "g", "o"]
+# algo2 = {"a", "r", "t"}
+# for algo in algo2:
+#     if algo in algo1:
+#         print("Yes")
+#     else:
+#         print("No")
--- atcoder/abc236/c/main.py
+++ atcoder/abc236/c/main.py
@@ -1,1 +1,9 @@
 # コードを記載
+N, M = map(int, input().split())
+S = list(map(str, input().split()))
+T = set(map(str, input().split()))
+for items in S:
+    if items in T:
+        print("Yes")
+    else:
+        print("No")
