f75660ec777417f0396324466b6218383d6407b7
shun198 <shunhiroseluvmri@gmail.com>
2022-09-23 08:16:27 +0900

    :+1: add abc158

---
+++ atcoder/abc158/b/main.py
@@ -0,0 +1,5 @@
+# コードを記載
+N,A,B = map(int,input().split())
+# N // (A+B) * A → Aの個数を求める
+# min(N % (A+B), A) → 余ったボールの個数を求める
+print(N // (A+B) * A + min(N % (A+B), A))
---
+++ atcoder/abc158/c/main.py
@@ -0,0 +1,12 @@
+# コードを記載
+A, B = map(int,input().split())
+# A-(A*0.08)からA+(A*0.08)までってこと？
+yen = 0
+
+for i in range(int(A/0.1),int(B/0.08)+1):
+        if i * 0.08 >= A:
+            yen = i
+            continue
+        else:
+            break
+print(yen)
